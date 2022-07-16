087bfbf4cdd9ecd67926bb599517f8c9f42c281a
Haikal Wahyudi <haikalwahyudi0@gmail.com>
2022-07-16 16:20:22 +0700

    Desain Pembayaran

--- app/Views/pesantiket/v_dpesantiket.php
+++ app/Views/pesantiket/v_dpesantiket.php
@@ -8,24 +8,55 @@
                   <h4 class="card-title">Pemesanan Tiket Kapal</h4>
                   <form id="example-form" action="#">
                     <div>
-                      <h3>Account</h3>
+                      <h3>Data Diri</h3>
                       <section>
-                        <h3>Account</h3>
+                        <h3>Masukkan Data Diri Anda</h3>
                         <div class="form-group">
-                          <label>Email address</label>
-                          <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email">
-                          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                          <label>Nama</label>
+                          <input type="text" class="form-control" placeholder="Masukkan Nama">
                         </div>
                         <div class="form-group">
-                          <label>Password</label>
-                          <input type="password" class="form-control" placeholder="Password">
+                          <label class="col-sm-6 col-form-label">Jenis Kelamin</label>
+                          <div class="col-sm-3">
+                            <div class="form-check form-check-secondary">
+                              <label class="form-check-label">
+                                <input type="radio" class="form-check-input" name="jk" id="membershipRadios1" value="Laki-Laki" checked>
+                                Laki-Laki
+                              </label>
+                            </div>
+                          </div>
+                          <div class="col-sm-3">
+                            <div class="form-check form-check-secondary">
+                              <label class="form-check-label">
+                                <input type="radio" class="form-check-input" name="jk" id="membershipRadios2" value="Perempuan">
+                                Perempuan
+                              </label>
+                            </div>
+                          </div>
                         </div>
                         <div class="form-group">
-                          <label>Confirm Password</label>
-                          <input type="password" class="form-control" placeholder="Confirm password">
+                          <label>Tempat Lahir</label>
+                          <input type="text" class="form-control" placeholder="Masukkan Tempat Lahir">
+                        </div>
+                        <div class="form-group">
+                          <label>Tanggal Lahir</label>
+                          <input type="date" class="form-control" placeholder="Masukkan Tanggal Lahir">
+                        </div>
+                        <div class="form-group">
+                          <label>NIK</label>
+                          <input type="number" class="form-control" placeholder="Masukkan NIK Anda">
+                        </div>
+                        <div class="form-group">
+                          <label>No Hp</label>
+                          <input type="number" class="form-control" placeholder="Masukkan Nomor Handphone">
+                        </div>
+                        <div class="form-group">
+                          <label>Alamat</label>
+                          <textarea class="form-control" rows="5" placeholder="Masukkan Alamat Anda"></textarea>
                         </div>
                       </section>
-                      <h3>Profile</h3>
+
+                      <h3>Jenis Kendaraan</h3>
                       <section>
                         <h3>Profile</h3>
                         <div class="form-group">
@@ -41,15 +72,43 @@
                           <input type="password" class="form-control" placeholder="Profession">
                         </div>
                       </section>
-                      <h3>Comments</h3>
+
+                      <h3>Pembayaran</h3>
                       <section>
-                        <h3>Comments</h3>
-                        <div class="form-group">
+                        <h3>Pilih Metode Pembayaran</h3>
+                        <!-- <div class="form-group">
                           <label>Comments</label>
                           <textarea class="form-control" rows="3"></textarea>
+                        </div> -->
+                  <div class="mt-4">
+                    <div class="accordion" id="accordion" role="tablist">
+                      <div class="card">
+                        <div class="card-header" role="tab" id="heading-2">
+                          <h6 class="mb-0">
+                            <a class="collapsed" data-bs-toggle="collapse" href="#collapse-2" aria-expanded="false" aria-controls="collapse-2">
+                              Bank
+                            </a>
+                          </h6>
                         </div>
+                        <div id="collapse-2" class="collapse" role="tabpanel" aria-labelledby="heading-2" data-parent="#accordion">
+                          <div class="card-body">
+                              <p>If while signing in to your account you see an error message, you can do the following</p>
+                            <ol class="ps-3 mt-4">
+                              <li>Check your network connection and try again</li>
+                              <li>Make sure your account credentials are correct while signing in</li>
+                              <li>Check whether your account is accessible in your region</li>
+                            </ol>
+                            <br>
+                            <p class="text-success">
+                              <i class="ti-alert-octagon me-2"></i>If the problem persists, you can contact our support.
+                            </p>
+                          </div>
+                        </div>
+                      </div> 
+                  </div>
                       </section>
-                      <h3>Finish</h3>
+
+                      <!-- <h3>Finish</h3>
                       <section>
                         <h3>Finish</h3>
                         <div class="form-check">
@@ -58,7 +117,8 @@
                             I agree with the Terms and Conditions.
                           </label>
                         </div>
-                      </section>
+                      </section> -->
+
                     </div>
                   </form>
                 </div>
